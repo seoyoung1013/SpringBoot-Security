2b4e8ebf2af161aaf343e727e9fc436962161e36
seoyoung1013 <lemi01@naver.com>
2023-10-16 00:03:21 +0900

    시큐리티 권한 처리

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.cos.security1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,6 @@ import com.cos.security1.repository.UserRepository;
 
 @Controller //View를 리턴하겠다!
 public class IndexController {
-	
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -50,6 +51,8 @@ public class IndexController {
         return "loginForm";
     }
     
+    
+    
     @GetMapping("/joinForm")
     public String joinForm(){
         return "joinForm";
@@ -61,10 +64,24 @@ public class IndexController {
     	user.setRole("ROLE_USER");
     	String rawPassword = user.getPassword();
     	String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+
     	user.setPassword(encPassword);
     	userRepository.save(user); //회원가입잘됨. 비밀번호 : 1234 => 시큐리티로 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안되어있음.
     	
         return "redirect:/loginForm";
+    }
+    
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+    	return "개인정보";
+    }
+    
+    @PreAuthorize("hasRole('ROLE_MANAGER')or hasRole('ROLE_ADMIN)")
+    //@PostAuthorize()
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+    	return "데이터정보";
     }
     
 }
