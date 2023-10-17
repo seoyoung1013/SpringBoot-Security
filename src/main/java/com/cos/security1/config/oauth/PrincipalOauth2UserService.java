98a61b84d26c23782e820dcb5cc7e9d59425ad1f
seoyoung1013 <lemi01@naver.com>
2023-10-17 23:30:15 +0900

    Authentication 객체가 가질 수 있는 2가지 타입

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -15,15 +15,18 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		
 		System.out.println("userRequest:"+userRequest);
-		System.out.println("getClientRegistration:"+userRequest.getClientRegistration());
+		System.out.println("getClientRegistration:"+userRequest.getClientRegistration()); //registrationId로 어떤 OAuth로 로그인 했는지 확인가능.
 		System.out.println("getAccessToken:"+userRequest.getAccessToken());
+		// 구글로그인 버튼 클릭 -> 구글로그인창 -> 로그인 완료 -> code를 리턴(Oauth-Client라이브러리)->AccessToken요청
+		// userRequest 정보 -> loadUser 함수 호출 -> 구글로부터 회원 프로필 받아준다.
 		System.out.println("getAttributes:"+super.loadUser(userRequest).getAttributes());
 		
+		OAuth2User oAuth2User = super.loadUser(userRequest); // google의 회원 프로필 조회
 		//회원가입을 강제로 진행
 		
 		return super.loadUser(userRequest);
-		/*OAuth2User oAuth2User = super.loadUser(userRequest); // google의 회원 프로필 조회
-
+		
+		/*
 		// code를 통해 구성한 정보
 		System.out.println("userRequest clientRegistration : " + userRequest.getClientRegistration());
 		// token을 통해 응답받은 회원정보
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,13 +2,17 @@ package com.cos.security1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 
@@ -20,6 +24,29 @@ public class IndexController {
 
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@GetMapping("/test/login")
+	public @ResponseBody String testLogin(
+			Authentication authentication,
+			@AuthenticationPrincipal PrincipalDetails userDetails) { //DI(의존성주입)
+		System.out.println("/test/login ================");
+		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+		System.out.println("authentication : " +principalDetails.getUser());
+		System.out.println("userDetails: " + userDetails.getUser());
+		return "세션 정보 확인";
+	}
+	
+	@GetMapping("/test/oauth/login")
+	public @ResponseBody String testOAuthLogin(
+			Authentication authentication,
+			@AuthenticationPrincipal OAuth2User oauth) { //DI(의존성주입)
+		System.out.println("/test/oauth/login ================");
+		OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+		System.out.println("authentication : " +oauth2User.getAttributes());
+		System.out.println("OAuth2User : " +oauth.getAttributes());
+		return "OAuth 세션 정보 확인";
+	}
+	
 	
     //localhost:8080/
     //localhost:8080
